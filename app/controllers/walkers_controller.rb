5adceff82cdafa5002934ab4da5ed76a6dadf09d
Namoneo <namoneo@icloud.com>
2016-10-09 23:59:31 +0200

    Walker is ready

--- app/controllers/walkers_controller.rb
+++ app/controllers/walkers_controller.rb
@@ -1,4 +1,54 @@
 class WalkersController < ApplicationController
+before_action :find_walker, only: [:show, :edit, :update, :destroy]
+
   def index
+    @walkers = Walker.all.order("created_at ASC")
   end
+
+  def new
+    @walker = Walker.new
+  end
+
+  def create
+
+    @walker = Walker.new( walker_params )
+
+    if @walker.save
+       redirect_to @walker
+    else
+       render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    find_walker
+
+    if @walker.update(walker_params)
+      redirect_to @walker
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @walker.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_walker
+      @walker = Walker.find(params[:id])
+  end
+
+  def walker_params
+    walker_params = params.require( :walker ).permit( :name, :desc, :image_url )
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+walker1 = Walker.create( { name: "Obama", image_url: "http://images.dinosaurpictures.org.rsz.io/iguanodon_b35b.jpg", desc: "Hi, I am morning dog walker. I will walk with your dog between 9am and 11am"} )
+walker2 = Walker.create( { name: "Maxima", image_url: "http://images.dinosaurpictures.org.rsz.io/mosasaurus_hoffmani_by_olorotitan-d4esmfm_5c48.jpg", desc: "Hi, I am afternoon dog walker. I will walk with your dog between 15pm and 17pm" } )
+walker3 = Walker.create( { name: "Lipsum", image_url: "http://images.dinosaurpictures.org.rsz.io/nyito_4f23.jpg", desc: "Hi, I am evening dog walker. I will walk with your dog between 9pm and 10pm" } )
